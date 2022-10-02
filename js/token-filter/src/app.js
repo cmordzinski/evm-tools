c938da1016f023bcf5fcd394c1fba7b177a7a286
cmordzinski <cmordzinski@gmail.com>
2022-10-02 15:26:52 -0500

    token-filter updates

--- js/token-filter/src/app.js
+++ js/token-filter/src/app.js
@@ -21,30 +21,25 @@ const {
     PAIR_CONTRACT_ABI,
 } = require('./config/constants.js');
 
-// The contract called here is the UniswapFlashQuery contract
-const getPairs = async () => {   
+const provider = new ethers.providers.WebSocketProvider(RPC_URL_WSS);
+
+async function getPairs() {   
     let allPoolInfo = [];
     let allTokens = [];
-    let output = [];
-    const provider = new ethers.providers.WebSocketProvider(RPC_URL_WSS);
     logger.info("connected to RPC")
     const uniswapQuery = new ethers.Contract(UNISWAP_QUERY_CONTRACT_ADDRESS, UNISWAP_QUERY_ABI, provider);
     logger.info("connected to contract")
-    // this will get the first 10 000 pairs in batches of 500
+    // this will get all pairs pairs from QUERY_START to QUERY_END in batches of 500
     for (let i = UNISWAP_QUERY_START; i < UNISWAP_QUERY_END; i += UNISWAP_BATCH_SIZE) {
         const pairs = (await uniswapQuery.functions.getPairsByIndexRange(PANCAKE_FACTORY_ADDRESS, i, i + UNISWAP_BATCH_SIZE))[0];
+        const L = allTokens.length // will be 0 in first run then 500, then 1000...
         // each pair item is formatted as: [token1,token2,pairAddress]
-        // for each pair in pairs we check whether token2 is busd or weth
-        const L = allTokens.length //will be 0 in first run then 500, then 1000...
         for (let pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
             const pair = pairs[pairIndex];
-            // token1 is always the main token being swapped, token2 is what people use to trade the token. BUSD, WBNB, WETH etc...
             let tokenAddress = pair[0]; //main token
-            let pairAddr;
-
-            // we check whether token2 (i.e pair[1]) is busd or weth because we are only focusing on pairs that contain either of these
+            let pairAddr = pair[2];
+            // token1 is always the main token being swapped, token2 is what people use to trade the token. BUSD, WBNB, WETH etc...
             if (pair[1] === WBNB_ADDRESS) { // if token2 is WBNB...
-                pairAddr = pair[2];
                 // if token has never been seen before we add it to allTokens and create a new allPoolInfo item
                 if (!allTokens.includes(tokenAddress)) {
                     allTokens[L + pairIndex] = tokenAddress;
@@ -58,7 +53,6 @@ const getPairs = async () => {
                     allPoolInfo[n].busdPair = pairAddr;
                 }
             } else if (pair[1] === BUSD_ADDRESS) { //if token2 is BUSD...
-                pairAddr = pair[2];
                 if (!allTokens.includes(tokenAddress)) {
                     allTokens[L + pairIndex] = tokenAddress
                     allPoolInfo[L + pairIndex] = {
@@ -71,35 +65,41 @@ const getPairs = async () => {
                     allPoolInfo[n].wethPair = pairAddr;
                 }
             } else {
+
                 continue;
             }
-
         }
         if (pairs.length < UNISWAP_BATCH_SIZE) {
-            logger.info("breaking...")
+            logger.info("that was the last breaking...")
             break
         }
     }
-    logger.info(`totalPairs fetched: ${allTokens.length}`);
+    logger.info(`total pairs fetched: ${allTokens.length}`);
     
+    // filter out any null entries that exist due to a token pair existing, but not being paired to WBNB or BUSD
     allTokens = allTokens.filter(function(value, index, arr) {
         return value != null;
     });
-
     allPoolInfo = allPoolInfo.filter(function(value, index, arr) {
         return value != null;
     });
+    logger.info(`pairs with WBNB or BUSD pair: ${allTokens.length}`);
 
-    logger.info('filtering out honeypots, tokens with high buy/sell tax, tokens with low reserves in the pair pool');
+    filterPairs(allTokens, allPoolInfo);
+};
 
+async function filterPairs(allTokens, allPoolInfo) {   
+    let output = [];
     const ethIn = ethers.utils.parseUnits("1", "ether");
-    
+    // 1% (0.01) fee tolerance to account for the dex fee
+    const feeTolerance = ethers.utils.parseUnits("0.01", "ether");
+    logger.info('filtering out honeypots, tokens with high buy/sell tax, tokens with low reserves in the pair pool');
     for (let i = 0; i < allTokens.length; i++) {
-        logger.info(`processing token [${allTokens[i]}]`);
-        var processedData = TP_CONTRACT.encodeFunctionData( //we have a 1% (0.01) fee tolerance because we're accounting for the dex Fee
-            'tokenToleranceCheck', [allTokens[i], ethIn, ethers.utils.parseUnits("0.01", "ether")]
+        logger.info(`processing token [${allTokens[i]}]: ${i}/${allTokens.length}`);
+        const processedData = TP_CONTRACT.encodeFunctionData( 
+            'tokenToleranceCheck', [allTokens[i], ethIn, feeTolerance]
         );
-        var checkTxn = {
+        const checkTxn = {
             from: BNB_RESERVE_ADDRESS,
             to: TP_CONTRACT_ADDRESS,
             data: processedData,
@@ -112,19 +112,18 @@ const getPairs = async () => {
         try {
             await provider.call(checkTxn)
         } catch (error) { 
-            logger.error('error calling tokenToleranceCheck(), honeypot or high tax');
+            logger.info('detected honeypot or high tax, skipping');
             continue;
         }
         // If there is a WBNB pair, get its reserves
         if (allPoolInfo[i].wethPair !== "") {
-            
             const wethPairContract = new ethers.Contract(allPoolInfo[i].wethPair, PAIR_CONTRACT_ABI, provider);
             let wethReserves = await wethPairContract.getReserves(); //if this doesnt throw an error we proceed to check minimum reserves amount
             if (wethReserves._reserve1 / (10 ** 18) > MIN_WBNB_RESERVES) {
                 //output[i] = [allPoolInfo[i].tokenAddress, allPoolInfo[i].busdPair, allPoolInfo[i].wethPair]; //if enough reserves we create a output item with [token, busdPair, wethPair] addresses
                 output[i] = ['"'+allPoolInfo[i].tokenAddress+'"'+",",];
             } else {
-                logger.error(`low WBNB reserves: ${allPoolInfo[i].tokenAddress}`);
+                logger.info('low WBNB reserves, skipping');
             }
         }
         // If there is a BUSD pair, get its reserves
@@ -135,7 +134,7 @@ const getPairs = async () => {
                 //output[i] = [allPoolInfo[i].tokenAddress, allPoolInfo[i].busdPair, allPoolInfo[i].wethPair]; //if enough reserves we create a output item with [token, busdPair, wethPair] addresses
                 output[i] = ['"'+allPoolInfo[i].tokenAddress+'"'+",",];
             } else {
-                logger.error(`low BUSD reserves: ${allPoolInfo[i].tokenAddress}`);
+                logger.info('low BUSD reserves, skipping');
             }
         }
 
@@ -147,14 +146,14 @@ const getPairs = async () => {
     //writeStream.write("[\n");
     output.forEach(value => writeStream.write(value+'\n'));
     writeStream.on('finish', () => {
-        logger.info(`wrote all the array data to file ${pathName}`);
+        logger.info(`wrote all filtered pairs to file ${pathName}`);
     });
     //writeStream.write("]\n");
     writeStream.on('error', (err) => {
-        logger.error(`There is an error writing the file ${pathName} => ${err}`)
+        logger.error(`there is an error writing the file ${pathName} => ${err}`)
     });
     writeStream.end();
     logger.info(allTokens.length);
-    }
+    };
 
 getPairs();
--- js/token-filter/src/config/constants.js
+++ js/token-filter/src/config/constants.js
@@ -1,37 +1,35 @@
 const {ethers} = require('ethers');
 
 const RPC_URL_WSS = "wss://ws-nd-654-414-664.p2pify.com/ae9f2cd14774753ae3150be26252ebbb";
+const MIN_WBNB_RESERVES = 5; // minumum amount of WBNB returned when calling getReserves() for pair to be included in output
+const MIN_BUSD_RESERVES = 1000; // minimum amount of BUSD returned when calling getReserves() for pair to be included in output
+const UNISWAP_QUERY_START = 50000; // UniswapFlasQuery.sol returns pairs in the same order each time in order of creation. 
+const UNISWAP_QUERY_END = 100000;   // There are too many pairs to get them all in one go, so we need to set the range we want to query
 
+// V2 factory address, along with the addresses of the 2 tokens we want to filter against for pairs
+// here, we are filtering for all XXX-WBNB and XXX-BUSD pairs
 const PANCAKE_FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73';
 const WBNB_ADDRESS = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
 const BUSD_ADDRESS = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
 
-const MIN_WBNB_RESERVES = 5;
-const MIN_BUSD_RESERVES = 1000;
-
-const PAIR_CONTRACT_ABI = require("./abi/PairContract.json");
-
-//----- Address and ABI of the UniswapFlashQuery.sol contract that we use to fetch all pairs from a certain exchange
+// Address and ABI of the UniswapFlashQuery.sol contract that we use to fetch all pairs from a certain exchange
 const UNISWAP_QUERY_CONTRACT_ADDRESS = '0xAe197E1C310AEC1c254bCB7998cdFd64541c9eef'
 const UNISWAP_QUERY_ABI = require("./abi/UniswapFlashQuery.json");
 
-// UniswapFlasQuery.sol returns pairs in the same order each time in order of creation. 
-// There are too many pairs to get them all in one go, so we need to set the range we want to query
-const UNISWAP_QUERY_START = 10000;
-const UNISWAP_QUERY_END = 20000;
-
-// Batch size to use when processing pairs pulled down
+// Batch size to use when getting pairs
 const UNISWAP_BATCH_SIZE = 500;
-//-----
 
-//----- Contract info for toleranceCheck.sol that checks for tax/honeypot 
+// Contract info for ToleranceCheck.sol that checks for tax/honeypot 
 const TP_CONTRACT_ADDRESS = '0x1298E04B39C8B5D79109F2e90079122C382885Ff';
 const toleranceCheckAbi = require("./abi/ToleranceCheck.json");
 const TP_CONTRACT = new ethers.utils.Interface(toleranceCheckAbi);
-// This is used when we simulate the tx to check for Honeypots/Tax. Simulating tx requires a "from" address with enough BNB, but doesnt
-// actually cost anything, so we just pass the BNB burn address
+
+// This is used when we simulate the tx to check for Honeypots/Tax. 
+// Simulating tx just requires a "from" address with enough BNB, so we pass the burn address
 const BNB_RESERVE_ADDRESS = '0x0000000000000000000000000000000000000000';
-//-------------
+
+// ABI of the V2 pair contract so we can call getReserves() on token pair addresses
+const PAIR_CONTRACT_ABI = require("./abi/PairContract.json");
 
 module.exports = {
         RPC_URL_WSS,
